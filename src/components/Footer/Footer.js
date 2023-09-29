8875aeb0be225d3dccadf2ecbc5752f463c7b701
Juan Pablo Martinez Pineda <jp.martinezp1@uniandes.edu.co>
2023-09-29 09:19:56 -0500

    Avance Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+import './Footer.css'; 
+
+function Footer() {
+    return ( 
+        <div className="footer">
+            <Row>
+                <p className="text-center">Contact us: +57 3102105253 - info@elaromamagico.com - @elaromamagico</p>
+            </Row>
+        </div>
+    );
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import Banner from './components/Banner/Banner';
+import Footer from './components/Footer/Footer'
 import reportWebVitals from './reportWebVitals';
 
 import Container from "react-bootstrap/Container";
@@ -12,7 +13,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <Container className="mt-3">
-    <Banner imageUrl={bannerImg}/>
+    <Banner imageUrl={bannerImg}></Banner>
+    <Footer></Footer>
   </Container>
 );
 
